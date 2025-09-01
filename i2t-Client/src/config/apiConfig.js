cdbf38d634eb79f16bba0ffd191f03b7c00ebb3e
Awi <caingletaouievyv@gmail.com>
2025-09-02 02:03:01 +0800

    Prepare frontend for Netlify

--- i2t-Client/src/config/apiConfig.js
+++ i2t-Client/src/config/apiConfig.js
@@ -1,6 +1,6 @@
 // i2t-client/src/config/apiConfig.js
 
-//export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5227";
-export const API_BASE_URL = "http://localhost:5227";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const OCR_SINGLE_URL = `${API_BASE_URL}/api/ocr`;
 export const OCR_MULTI_URL = `${API_BASE_URL}/api/ocr/UploadMultipleImages`;
